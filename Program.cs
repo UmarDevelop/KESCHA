3995867598f9bf0fda1f862f46ed8739fc0c46f9
UmarDevelop <umar180396@gmail.com>
2023-10-22 21:18:45 +0100

    LEARNING COMPARISON

--- Program.cs
+++ Program.cs
@@ -16,5 +16,42 @@ System.Console.WriteLine($"if subtraction two numbers each other rusult is: {Num
 System.Console.WriteLine($"if multiple two numbers each other rusult is: {Number1} * {number2} = {result}");
 System.Console.WriteLine($"if divide two numbers each other rusult is: {Number1} / {number2} = {result1}");
 
+decimal firstNumber;
+decimal secondNumber;
+string name;
+string greeting = "Assalomu Aleykum";
+System.Console.WriteLine("Hi welcome to our website\nWhat is your name ?");
+name = Console.ReadLine();
+System.Console.WriteLine($"{greeting} {name}");
+
+System.Console.WriteLine("Lets compare numbers !!!");
+System.Console.WriteLine("Enter 1st number which you wanna compare: ");
+firstNumber = Convert.ToDecimal(Console.ReadLine());
+System.Console.WriteLine("Enter 2nd nuber which you wanna compare: ");
+secondNumber = Convert.ToDecimal(Console.ReadLine());
+System.Console.WriteLine($"1st number is bigger than 2nd {firstNumber > secondNumber}");
+System.Console.WriteLine($"1st number is samll than 2nd number {firstNumber < secondNumber}");
+System.Console.WriteLine($"1st number is equal to 2nd number {firstNumber == secondNumber}");
+System.Console.WriteLine($"1st number is small or equal than 2nd number {firstNumber <= secondNumber}");
+System.Console.WriteLine($"1st number is bigger or equal than 2nd number {firstNumber >= secondNumber}");
+System.Console.WriteLine($"1st and 2nd numbers are not equal {firstNumber != secondNumber}");
+
+int keschasAge = 5;
+int myAge;
+System.Console.WriteLine("lets compare our ages\n how old are you ?");
+myAge = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"great {name} your age is: {myAge}");
+System.Console.WriteLine($"my age is: {keschasAge}");
+int difer = myAge - keschasAge;
+System.Console.WriteLine($"Our age difference is:{difer}");
+System.Console.WriteLine($"Umar is bigger than kescha {myAge > keschasAge}");
+System.Console.WriteLine($"Umar is small than kescha {myAge < keschasAge}");
+System.Console.WriteLine($"Umar's and kescha's age is equal {myAge == keschasAge}");
+System.Console.WriteLine($"Umar is bigger or equal than kescha {myAge >= keschasAge}");
+System.Console.WriteLine($"Umar is small or equal than kescha {myAge <= keschasAge}");
+System.Console.WriteLine($"Umar's and kescha's age are not equal {myAge != keschasAge}");
+
+
+
     
   
